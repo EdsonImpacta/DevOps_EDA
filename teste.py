b3378248e1008d1aae329e3c7067072386a3ae7c
EdsonImpacta <edson.bezerra@aluno.faculdadeimpacta.com.br>
2020-03-13 11:33:30 -0300

    commit

--- teste.py
+++ teste.py
@@ -1,6 +1,11 @@
 import pytest
 from principal import somar
+from principal import subtrair
 
 
 def test_soma():
     assert somar(2, 3) == 5
+
+
+def test_subtrair():
+    assert subtrair(5, 2) == 3
